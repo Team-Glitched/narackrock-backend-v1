8e18828aa55e577302cc8e317d91509ab09070d6
parafara <leebadagyumin0129@gmail.com>
2026-06-18 19:12:08 +0900

    initial commit

---
+++ src/main/java/glitched/adlips/AdlipsBackendApplication.java
@@ -0,0 +1,13 @@
+package glitched.adlips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdlipsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdlipsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/glitched/adlips/AdlipsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package glitched.adlips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdlipsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

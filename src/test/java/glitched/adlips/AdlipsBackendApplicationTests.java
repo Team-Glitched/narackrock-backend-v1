0676e6fe83674e3d98ae4b866297c3486a832e98
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-06-30 23:30:32 +0900

    test: 토큰 서명키 설정 검증 추가

--- src/test/java/glitched/adlips/AdlipsBackendApplicationTests.java
+++ src/test/java/glitched/adlips/AdlipsBackendApplicationTests.java
@@ -3,7 +3,7 @@ package glitched.adlips;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "app.auth.token-secret=test-only-secret-value-32-characters")
 class AdlipsBackendApplicationTests {
 
     @Test

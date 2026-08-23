f47667cd1dc49652bfba567c993ae1cabb0afadc
윤재준 <fruitman200912@gmail.com>
2026-08-23 13:58:35 +0900

    fix: 협업 추천 유스케이스 생성자 시그니처 컴파일 오류 수정

--- src/main/java/glitched/adlips/config/UserApplicationConfiguration.java
+++ src/main/java/glitched/adlips/config/UserApplicationConfiguration.java
@@ -21,6 +21,7 @@ import glitched.adlips.application.user.profile.usecase.ProfileImageUpdateUseCas
 import glitched.adlips.application.user.profile.usecase.ProfileMuteToggleUseCase;
 import glitched.adlips.application.user.profile.usecase.ProfileShareUseCase;
 import glitched.adlips.application.user.profile.usecase.ProfileUpdateUseCase;
+import glitched.adlips.application.user.relation.port.out.CollaborationUserQueryPort;
 import glitched.adlips.application.user.relation.port.out.FollowRepositoryPort;
 import glitched.adlips.application.user.relation.port.out.ProfileQueryPort;
 import glitched.adlips.application.user.relation.usecase.FollowCancelUseCase;
@@ -202,11 +203,12 @@ public class UserApplicationConfiguration {
     RecommendedUserGetListUseCase recommendedUserGetListUseCase(
             ProfileQueryPort profileQuery,
             FollowRepositoryPort followRepository,
+            CollaborationUserQueryPort collaborationUserQueryPort,
             MediaFileRepositoryPort mediaFileRepository,
             TransactionRunner transactionRunner
     ) {
         return new RecommendedUserGetListUseCase(
-                profileQuery, followRepository, mediaFileRepository, transactionRunner);
+                profileQuery, followRepository, collaborationUserQueryPort, mediaFileRepository, transactionRunner);
     }
 
     @Bean

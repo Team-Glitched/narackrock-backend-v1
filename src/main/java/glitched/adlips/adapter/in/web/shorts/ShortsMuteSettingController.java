758136ebfb627ef46c4a5bb564d581faf1ed2c88
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-09-15 08:45:48 +0900

    docs: ShortsMuteSettingController Swagger 문서화 추가

--- src/main/java/glitched/adlips/adapter/in/web/shorts/ShortsMuteSettingController.java
+++ src/main/java/glitched/adlips/adapter/in/web/shorts/ShortsMuteSettingController.java
@@ -1,5 +1,7 @@
 package glitched.adlips.adapter.in.web.shorts;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import glitched.adlips.adapter.in.web.ApiResponse;
 import glitched.adlips.adapter.in.web.dto.ShortsMuteResponse;
 import glitched.adlips.adapter.in.web.user.AuthenticatedUserResolver;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "숏폼 음소거", description = "숏폼 음소거 설정 API")
+@SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/api/v1/shorts")
 public class ShortsMuteSettingController {
 

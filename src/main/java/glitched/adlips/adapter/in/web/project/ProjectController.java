d13b76bf492e05bd113efc7c8aa3de34a9606fbb
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-09-15 08:39:36 +0900

    docs: ProjectController Swagger 문서화 추가

--- src/main/java/glitched/adlips/adapter/in/web/project/ProjectController.java
+++ src/main/java/glitched/adlips/adapter/in/web/project/ProjectController.java
@@ -1,5 +1,8 @@
 package glitched.adlips.adapter.in.web.project;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import glitched.adlips.adapter.in.web.ApiResponse;
 import glitched.adlips.adapter.in.web.user.AuthenticatedUserResolver;
 import glitched.adlips.application.project.dto.request.ProjectCreateRequest;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "프로젝트", description = "프로젝트 API")
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/v1/projects")
 public class ProjectController {

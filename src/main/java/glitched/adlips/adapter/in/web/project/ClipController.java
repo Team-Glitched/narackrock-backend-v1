9774c995093ed3adc8e27af661b76e5d78949df7
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-09-15 08:39:00 +0900

    docs: ClipController Swagger 문서화 추가

--- src/main/java/glitched/adlips/adapter/in/web/project/ClipController.java
+++ src/main/java/glitched/adlips/adapter/in/web/project/ClipController.java
@@ -1,5 +1,8 @@
 package glitched.adlips.adapter.in.web.project;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import glitched.adlips.adapter.in.web.ApiResponse;
 import glitched.adlips.adapter.in.web.user.AuthenticatedUserResolver;
 import glitched.adlips.application.project.dto.request.AudioClipCreateRequest;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "클립", description = "오디오 및 MIDI 클립 API")
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/v1/projects/{projectId}/tracks/{trackId}/clips")
 public class ClipController {

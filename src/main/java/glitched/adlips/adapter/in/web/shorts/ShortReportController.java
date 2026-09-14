6a1ad7ca0db154e7627c858506748e4ee234c747
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-09-15 08:45:06 +0900

    docs: ShortReportController Swagger 문서화 추가

--- src/main/java/glitched/adlips/adapter/in/web/shorts/ShortReportController.java
+++ src/main/java/glitched/adlips/adapter/in/web/shorts/ShortReportController.java
@@ -1,5 +1,7 @@
 package glitched.adlips.adapter.in.web.shorts;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import glitched.adlips.adapter.in.web.ApiResponse;
 import glitched.adlips.adapter.in.web.dto.ShortReportRequest;
 import glitched.adlips.adapter.in.web.dto.ShortReportResponse;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "숏폼 신고", description = "숏폼 신고 API")
+@SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/api/v1/shorts")
 public class ShortReportController {
 

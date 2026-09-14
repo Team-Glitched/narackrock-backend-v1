3fc365fd4a149d40fde4d0c7c6ce1c902c7ee98f
Yang jeongwoo <yangjeongwoo3283@gmail.com>
2026-09-15 08:40:00 +0900

    docs: TrackDeleteController Swagger 문서화 추가

--- src/main/java/glitched/adlips/adapter/in/web/project/TrackDeleteController.java
+++ src/main/java/glitched/adlips/adapter/in/web/project/TrackDeleteController.java
@@ -1,5 +1,8 @@
 package glitched.adlips.adapter.in.web.project;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import glitched.adlips.adapter.in.web.ApiResponse;
 import glitched.adlips.adapter.in.web.user.AuthenticatedUserResolver;
 import glitched.adlips.application.project.dto.request.TrackDeleteRequest;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "트랙", description = "트랙 삭제 API")
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/v1/tracks")
 public class TrackDeleteController {
